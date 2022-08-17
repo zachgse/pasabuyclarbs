691edfb66cbb0c349f3574fe5a24389155b59e2c
Zach Gabriel S. Estrella <55230769+zachgse@users.noreply.github.com>
2022-08-17 19:27:02 +0800

    Updated responsiveness of owl carousel

--- js/script.js
+++ js/script.js
@@ -6,5 +6,17 @@ $(document).ready(function () {
         nav: true,
         autoplay: true,
         navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
     });  
 });
